958900f681df8c0b8833cbab99e6c2372b362b6e
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-02 16:01:22 +0100

    schema procedimento

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//chiedo all'utente il nome
+
+//chiedo all'utente il cognome
+
+//chiedo all'utente il colore preferito
+
+//per stampare la pass richiamo un oggetto
+//dell'html tramite un id e assegno un valore
+//alla sua variabile di defoult innerHTML
+
