94b09eced35f173fe28fc06634d413954cdcc774
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-02 16:48:42 +0100

    Stampa pass

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 //chiedo all'utente il nome
+let userName = prompt('Inserisci il tuo nome');
 
 //chiedo all'utente il cognome
+let userLastName = prompt('Inserisci il tuo cognome');
 
 //chiedo all'utente il colore preferito
+let userColor = prompt('Inserisci il tuo colore preferito');
 
 //per stampare la pass richiamo un oggetto
 //dell'html tramite un id e assegno un valore
 //alla sua variabile di defoult innerHTML
+let htmlElement = document.getElementById('password');
 
+//htmlElement.innerHTML= userName + userLastName + userColor;
+htmlElement.innerHTML= '${userName}${userLastName}${userColor}';
+
+console.log(htmlElement.innerHTML);
